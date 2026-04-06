a322b2d8aa4c734b175251765ed85ccab3571c8c
Hairum Qureshi <hqureshi@udel.edu>
2026-04-06 11:00:30 -0400

    created Navbar

---
+++ apps/frontend/src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+	return (
+		<div className="flex text-white items-center px-4 py-3 bg-blue-700">
+			<h1 className="text-2xl font-bold">DocQuery</h1>
+			<nav className="ml-10 space-x-4">
+				<Link to="/" className="hover:underline">
+					Home
+				</Link>
+				<Link to="/about" className="hover:underline">
+					About
+				</Link>
+				<Link to="/uploaded-documents" className="hover:underline">
+					Uploaded Documents
+				</Link>
+				<Link to="/conversations" className="hover:underline">
+					Conversations
+				</Link>
+			</nav>
+		</div>
+	);
+}
