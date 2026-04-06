ff8ac4105bc890e59ea3aaad2b06c3dbc3ad7b84
Hairum Qureshi <hqureshi@udel.edu>
2026-04-06 11:00:50 -0400

    added Navbar and created 'uploaded-documents' route

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -3,13 +3,17 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import "../css/index.css";
+import UploadedDocuments from "../pages/UploadedDocuments";
+import Navbar from "./Navbar";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
+				<Route path="/uploaded-documents" element={<UploadedDocuments />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
