05c6039ff5008ee0a6894989655ee0c68941deaa
Hairum Qureshi <hqureshi@udel.edu>
2026-04-06 11:15:47 -0400

    added 'Give Access' button and a section for listing users that have access to the documents; also moved tables to separate components

--- apps/frontend/src/pages/UploadedDocuments.tsx
+++ apps/frontend/src/pages/UploadedDocuments.tsx
@@ -1,4 +1,5 @@
-import { AiFillFilePdf } from "react-icons/ai";
+import AccessTable from "../components/AccessTable";
+import UploadsTable from "../components/UploadsTable";
 
 export default function UploadedDocuments() {
 	return (
@@ -14,159 +15,23 @@ export default function UploadedDocuments() {
 					your existing chats.
 				</p>
 			</div>
-			<div>
-				<button className="my-5 py-2 px-4 bg-blue-800 text-white rounded-md">
+			<div className="w-full flex items-center gap-3">
+				<button className="my-5 hover:cursor-pointer py-2 px-4 bg-blue-800 text-white rounded-md">
 					Upload Documents
 				</button>
+				<button className="my-5 hover:cursor-pointer py-2 px-4 bg-green-700 text-white rounded-md">
+					Give Access
+				</button>
 			</div>
-			<div>
-				<div className="overflow-hidden rounded-md border border-gray-300">
-					<table className="min-w-full table-fixed">
-						<thead>
-							<tr className="bg-gray-100">
-								<th className="p-3 text-left w-1/3">Name</th>
-								<th className="p-3 text-left w-1/3">Uploaded Date</th>
-								<th className="p-3 text-left w-1/3">Uploaded By</th>
-								<th className="p-3 text-left w-1/3">Actions</th>
-							</tr>
-						</thead>
-
-						<tbody>
-							<tr className="hover:bg-gray-50 cursor-pointer">
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<AiFillFilePdf className="text-3xl text-slate-500 bg-gray-200 p-1 rounded-md flex items-center justify-center" />
-										<p>Some File.pdf</p>
-									</div>
-								</td>
-								<td className="p-4">March 14th, 2024 at 3:00 PM</td>
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<img
-											src="https://i.pinimg.com/474x/9c/0f/06/9c0f06b14aba220811331c49718d6b93.jpg"
-											className="w-8 h-8 rounded-full"
-											alt="User Avatar"
-										/>
-										<p>Mark Joe</p>
-									</div>
-								</td>
-								<td className="p-4 text-red-600 hover:underline cursor-pointer font-semibold">
-									Delete
-								</td>
-							</tr>
-							<tr className="border-t border-gray-300 hover:bg-gray-50 cursor-pointer">
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<AiFillFilePdf className="text-3xl text-slate-500 bg-gray-200 p-1 rounded-md flex items-center justify-center" />
-										<p>Some File.pdf</p>
-									</div>
-								</td>
-								<td className="p-4">March 14th, 2024 at 3:00 PM</td>
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<img
-											src="https://i.pinimg.com/474x/9c/0f/06/9c0f06b14aba220811331c49718d6b93.jpg"
-											className="w-8 h-8 rounded-full"
-											alt="User Avatar"
-										/>
-										<p>Alex Joe</p>
-									</div>
-								</td>
-								<td className="p-4 text-red-600 hover:underline cursor-pointer font-semibold">
-									Delete
-								</td>
-							</tr>
-							<tr className="border-t border-gray-300 hover:bg-gray-50 cursor-pointer">
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<AiFillFilePdf className="text-3xl text-slate-500 bg-gray-200 p-1 rounded-md flex items-center justify-center" />
-										<p>Some File.pdf</p>
-									</div>
-								</td>
-								<td className="p-4">March 14th, 2024 at 3:00 PM</td>
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<img
-											src="https://i.pinimg.com/474x/9c/0f/06/9c0f06b14aba220811331c49718d6b93.jpg"
-											className="w-8 h-8 rounded-full"
-											alt="User Avatar"
-										/>
-										<p>Enid Joe</p>
-									</div>
-								</td>
-								<td className="p-4 text-red-600 hover:underline cursor-pointer font-semibold">
-									Delete
-								</td>
-							</tr>
-							<tr className="border-t border-gray-300 hover:bg-gray-50 cursor-pointer">
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<AiFillFilePdf className="text-3xl text-slate-500 bg-gray-200 p-1 rounded-md flex items-center justify-center" />
-										<p>Some File.pdf</p>
-									</div>
-								</td>
-								<td className="p-4">March 14th, 2024 at 3:00 PM</td>
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<img
-											src="https://i.pinimg.com/474x/9c/0f/06/9c0f06b14aba220811331c49718d6b93.jpg"
-											className="w-8 h-8 rounded-full"
-											alt="User Avatar"
-										/>
-										<p>Sam Doe</p>
-									</div>
-								</td>
-								<td className="p-4 text-red-600 hover:underline cursor-pointer font-semibold">
-									Delete
-								</td>
-							</tr>
-							<tr className="border-t border-gray-300 hover:bg-gray-50 cursor-pointer">
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<AiFillFilePdf className="text-3xl text-slate-500 bg-gray-200 p-1 rounded-md flex items-center justify-center" />
-										<p>Some File.pdf</p>
-									</div>
-								</td>
-								<td className="p-4">March 14th, 2024 at 3:00 PM</td>
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<img
-											src="https://i.pinimg.com/474x/9c/0f/06/9c0f06b14aba220811331c49718d6b93.jpg"
-											className="w-8 h-8 rounded-full"
-											alt="User Avatar"
-										/>
-										<p>Jane Doe</p>
-									</div>
-								</td>
-								<td className="p-4 text-red-600 hover:underline cursor-pointer font-semibold">
-									Delete
-								</td>
-							</tr>
-							<tr className="border-t border-gray-300 hover:bg-gray-50 cursor-pointer">
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<AiFillFilePdf className="text-3xl text-slate-500 bg-gray-200 p-1 rounded-md flex items-center justify-center" />
-										<p>Some File.pdf</p>
-									</div>
-								</td>
-								<td className="p-4">March 14th, 2024 at 3:00 PM</td>
-								<td className="p-4">
-									<div className="flex items-center gap-2">
-										<img
-											src="https://i.pinimg.com/474x/9c/0f/06/9c0f06b14aba220811331c49718d6b93.jpg"
-											className="w-8 h-8 rounded-full"
-											alt="User Avatar"
-										/>
-										<p>John Doe</p>
-									</div>
-								</td>
-								<td className="p-4 text-red-600 hover:underline cursor-pointer font-semibold">
-									Delete
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+			<div className="w-full my-4">
+				<h3 className="font-semibold text-2xl text-blue-700 my-6">
+					Who currently has access to your documents?
+				</h3>
+				<AccessTable />
+			</div>
+			<div className="w-full my-4">
+				<h3 className="font-semibold text-2xl text-blue-700 my-6">Files</h3>
+				<UploadsTable />
 			</div>
 		</div>
 	);
