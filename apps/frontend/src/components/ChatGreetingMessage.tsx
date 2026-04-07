073690e437706926c8478144c616b2fd9cdc10b8
Hairum Qureshi <hqureshi@udel.edu>
2026-04-06 23:23:46 -0400

    created component containing the chat greeting message

---
+++ apps/frontend/src/components/ChatGreetingMessage.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export default function ChatGreetingMessage() {
+	return (
+		<div className="text-left space-y-4 w-3xl">
+			<h2 className="font-bold text-blue-700 text-5xl">Hello John Doe,</h2>
+			<h3 className="text-3xl">
+				I'm your helpful assistant, <span className="text-sky-600">Docent</span>
+			</h3>
+			<h3>
+				Feel free to click on any of your existing conversations and resume
+				chatting with me. I'm here to help you learn your documents and answer
+				any questions you have. If you don't have any conversations yet, feel
+				free to start a new one by clicking{" "}
+				<Link to="/new" className="text-blue-500 underline">
+					here
+				</Link>
+				!
+			</h3>
+		</div>
+	);
+}
