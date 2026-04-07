f3cb4f730ff50e25b6332311d7a9f7e37d429496
Hairum Qureshi <hqureshi@udel.edu>
2026-04-06 23:23:28 -0400

    moved chat greeting message to a separate component and added conditional logic to show/hide it based on whether or not the Convo ID is in the URL

--- apps/frontend/src/pages/Conversations.tsx
+++ apps/frontend/src/pages/Conversations.tsx
@@ -1,31 +1,16 @@
-import { Link } from "react-router-dom";
 import InboxSideBar from "../components/InboxSideBar";
+import ChatGreetingMessage from "../components/ChatGreetingMessage";
+import { useParams } from "react-router-dom";
 
 export default function Conversations() {
+	const { convoID } = useParams();
+
 	return (
 		<div className="w-full h-screen flex">
 			<InboxSideBar />
 			<div className="w-[95%] min-h-screen bg-gray-100 overflow-y-auto">
 				<div className="flex items-center justify-center h-screen flex-col">
-					<div className="text-left space-y-4 w-3xl">
-						<h2 className="font-bold text-blue-700 text-5xl">
-							Hello John Doe,
-						</h2>
-						<h3 className="text-3xl">
-							I'm your helpful assistant,{" "}
-							<span className="text-sky-600">Docent</span>
-						</h3>
-						<h3>
-							Feel free to click on any of your existing conversations and
-							resume chatting with me. I'm here to help you learn your documents
-							and answer any questions you have. If you don't have any
-							conversations yet, feel free to start a new one by clicking{" "}
-							<Link to="/new" className="text-blue-500 underline">
-								here
-							</Link>
-							!
-						</h3>
-					</div>
+					{convoID ? <></> : <ChatGreetingMessage />}
 				</div>
 			</div>
 		</div>
