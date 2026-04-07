d3c1505d6bc25d1d0d6caabd00249d1e5b20f126
Hairum Qureshi <hqureshi@udel.edu>
2026-04-07 00:10:45 -0400

    fixed wording

--- apps/frontend/src/components/ChatGreetingMessage.tsx
+++ apps/frontend/src/components/ChatGreetingMessage.tsx
@@ -10,8 +10,8 @@ export default function ChatGreetingMessage() {
 			<h3>
 				Feel free to click on any of your existing conversations and resume
 				chatting with me. I'm here to help you learn your documents and answer
-				any questions you have. If you don't have any conversations yet, feel
-				free to start a new one by clicking{" "}
+				any questions you have about them. If you don't have any conversations
+				yet, feel free to start a new one by clicking{" "}
 				<Link to="/new" className="text-blue-500 underline">
 					here
 				</Link>
