f23af3e446635ec7e94330ead83265fbb9436fb0
Hairum Qureshi <hqureshi@udel.edu>
2026-04-07 00:13:58 -0400

    added conditional to not show the last few sentences if the user is viewing the message in an existing conversation since it wouldn't make sense

--- apps/frontend/src/components/ChatGreetingMessage.tsx
+++ apps/frontend/src/components/ChatGreetingMessage.tsx
@@ -1,6 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function ChatGreetingMessage() {
+	const { convoID } = useParams();
+
 	return (
 		<div className="text-left space-y-4 w-3xl">
 			<h2 className="font-bold text-blue-700 text-5xl">Hello John Doe,</h2>
@@ -10,12 +12,17 @@ export default function ChatGreetingMessage() {
 			<h3>
 				Feel free to click on any of your existing conversations and resume
 				chatting with me. I'm here to help you learn your documents and answer
-				any questions you have about them. If you don't have any conversations
-				yet, feel free to start a new one by clicking{" "}
-				<Link to="/new" className="text-blue-500 underline">
-					here
-				</Link>
-				!
+				any questions you have about them.{" "}
+				{!convoID && (
+					<>
+						If you don't have any conversations yet, feel free to start a new
+						one by clicking{" "}
+						<Link to="/new" className="text-blue-500 underline">
+							here
+						</Link>
+						!
+					</>
+				)}
 			</h3>
 		</div>
 	);
