c27e2020b35d1bd4caa5e9d4a11455daac544271
Hairum Qureshi <hqureshi@udel.edu>
2026-04-07 00:17:10 -0400

    added code for rendering the ConversationDocuments component if the conversation URL includes 'documents'

--- apps/frontend/src/pages/Conversations.tsx
+++ apps/frontend/src/pages/Conversations.tsx
@@ -1,16 +1,33 @@
 import InboxSideBar from "../components/InboxSideBar";
 import ChatGreetingMessage from "../components/ChatGreetingMessage";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import ChatInterface from "../components/ChatInterface";
+import ConversationDocuments from "../components/ConversationDocuments";
 
 export default function Conversations() {
 	const { convoID } = useParams();
+	const location = useLocation();
+
+	const locationIncludesDocuments = location.pathname.includes("/documents");
 
 	return (
 		<div className="w-full h-screen flex">
 			<InboxSideBar />
 			<div className="w-[95%] min-h-screen bg-gray-100 overflow-y-auto">
-				<div className="flex items-center justify-center h-screen flex-col">
-					{convoID ? <></> : <ChatGreetingMessage />}
+				<div
+					className={
+						convoID ? "" : "flex items-center justify-center h-screen flex-col"
+					}
+				>
+					{convoID ? (
+						!locationIncludesDocuments ? (
+							<ChatInterface />
+						) : (
+							<ConversationDocuments />
+						)
+					) : (
+						<ChatGreetingMessage />
+					)}
 				</div>
 			</div>
 		</div>
