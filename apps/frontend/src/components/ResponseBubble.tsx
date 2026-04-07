7c819334c597eb0708cc879ca8c21f98dc6c4a7e
Hairum Qureshi <hqureshi@udel.edu>
2026-04-06 23:55:56 -0400

    created ResponseBubble interface

---
+++ apps/frontend/src/components/ResponseBubble.tsx
@@ -0,0 +1,9 @@
+export default function ResponseBubble() {
+	return (
+		<div className="bg-gray-200 p-2 rounded-md">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+			corrupti nihil enim velit officiis deleniti est at esse maiores? Dolores
+			minima, animi quam in sit labore ducimus deleniti architecto ab.
+		</div>
+	);
+}
