1808bf49bfb83867f421cfdd5a293636ffe5aeee
Hairum Qureshi <hqureshi@udel.edu>
2026-04-08 11:27:58 -0400

    added flex-shrink-0 class to prevent shrinking PDF icon width for long file names

--- apps/frontend/src/components/UploadedPDF.tsx
+++ apps/frontend/src/components/UploadedPDF.tsx
@@ -3,9 +3,11 @@ import { AiFillFilePdf } from "react-icons/ai";
 import { IoMdClose } from "react-icons/io";
 
 export default function UploadedPDF({
+	fileName,
 	reducePadding = false,
 	showRemove = false
 }: {
+	fileName: string;
 	reducePadding?: boolean;
 	showRemove?: boolean;
 }) {
@@ -13,10 +15,10 @@ export default function UploadedPDF({
 		<div
 			className={`flex items-center gap-2 p-3 bg-white rounded-lg border border-blue-200 hover:shadow-md transition-shadow cursor-pointer ${reducePadding ? "py-2" : ""}`}
 		>
-			<div className="flex items-center justify-center w-12 h-12 bg-blue-100 rounded-md">
+			<div className="flex items-center justify-center w-12 h-12 bg-blue-100 rounded-md shrink-0">
 				<AiFillFilePdf className="text-2xl text-blue-600" />
 			</div>
-			<p className="text-gray-800 font-medium truncate">Some File.pdf</p>
+			<p className="text-gray-800 font-medium truncate">{fileName}</p>
 			<p
 				className={`${showRemove ? "ml-auto text-gray-500 flex items-center" : "ml-auto text-gray-500 flex"}`}
 			>
