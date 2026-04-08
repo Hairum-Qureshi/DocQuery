1dd4327fc1026f42c48bc8e72533813f2e90bf99
Hairum Qureshi <hqureshi@udel.edu>
2026-04-08 17:35:19 -0400

    uncommented auth-related imports and also replaced process.env code with configService

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,19 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { FirebaseModule } from './firebase/firebase.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ConversationModule } from './conversation/conversation.module';
 
 @Module({
   imports: [
-    // AuthModule,
-    // FirebaseModule,
+    AuthModule,
+    FirebaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     CloudinaryModule,
-    // MongooseModule.forRoot(process.env.MONGO_URI!),
+    ConversationModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
