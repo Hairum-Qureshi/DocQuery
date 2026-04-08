e83ed9925ee90239c4fcd163eaba3e63ce925895
Hairum Qureshi <hqureshi@udel.edu>
2026-04-07 20:44:44 -0400

    added optional reducePadding and showRemove props to component and added styling to show the remove button if true

--- apps/frontend/src/components/UploadedPDF.tsx
+++ apps/frontend/src/components/UploadedPDF.tsx
@@ -1,15 +1,29 @@
 import { FaDownload } from "react-icons/fa6";
 import { AiFillFilePdf } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
 
-export default function UploadedPDF() {
+export default function UploadedPDF({
+	reducePadding = false,
+	showRemove = false
+}: {
+	reducePadding?: boolean;
+	showRemove?: boolean;
+}) {
 	return (
-		<div className="flex items-center gap-2 p-3 bg-white rounded-lg border border-blue-200 hover:shadow-md transition-shadow cursor-pointer">
+		<div
+			className={`flex items-center gap-2 p-3 bg-white rounded-lg border border-blue-200 hover:shadow-md transition-shadow cursor-pointer ${reducePadding ? "py-2" : ""}`}
+		>
 			<div className="flex items-center justify-center w-12 h-12 bg-blue-100 rounded-md">
 				<AiFillFilePdf className="text-2xl text-blue-600" />
 			</div>
 			<p className="text-gray-800 font-medium truncate">Some File.pdf</p>
-			<p className="ml-auto text-gray-500">
+			<p
+				className={`${showRemove ? "ml-auto text-gray-500 flex items-center" : "ml-auto text-gray-500 flex"}`}
+			>
 				<FaDownload />
+				{showRemove && (
+					<IoMdClose className="ml-2 text-2xl text-red-500 hover:text-red-700" />
+				)}
 			</p>
 		</div>
 	);
