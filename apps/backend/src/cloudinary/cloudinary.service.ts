934432ee3cffaa95f325cc2d61c90af554dff6f4
Hairum Qureshi <hqureshi@udel.edu>
2026-04-08 21:16:14 -0400

    added code to handle spacing in file names so it's replaced with dashes

--- apps/backend/src/cloudinary/cloudinary.service.ts
+++ apps/backend/src/cloudinary/cloudinary.service.ts
@@ -7,8 +7,38 @@ export class CloudinaryService {
     @Inject('Cloudinary') private readonly cloudinary: typeof Cloudinary,
   ) {}
 
-  uploadPDFs(files: Express.Multer.File[]) {
-    console.log(files);
-    // Implement your upload logic here
+  uploadPDFs(
+    files: Express.Multer.File[],
+    currUserID: string,
+    conversationID: string,
+  ) {
+    for (const file of files) {
+      const DOCUMENT_NAME =
+        `${currUserID}_${file.originalname.split('.')[0]}`.replace(/\s+/g, '-');
+      const FOLDER_NAME = `${currUserID}_PDF_Uploads/${conversationID}`;
+
+      new Promise((resolve) => {
+        Cloudinary.uploader
+          .upload_stream(
+            { public_id: DOCUMENT_NAME, folder: FOLDER_NAME },
+            (error, uploadResult) => {
+              if (error) {
+                return error;
+              }
+
+              return resolve(uploadResult);
+            },
+          )
+          .end(file.buffer);
+      })
+        .then((uploadResult: any) => {
+          console.log(
+            `Buffer upload_stream wth promise success - ${uploadResult.public_id}`,
+          );
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
   }
 }
