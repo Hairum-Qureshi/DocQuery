3ad8e74b73b61483049e2ac6a95df4a02e6f719a
Hairum Qureshi <hqureshi@udel.edu>
2026-04-08 20:25:34 -0400

    added Conversation and Message schemas

---
+++ apps/backend/src/schemas/Conversation.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class Conversation {
+  @Prop({ ref: 'User' })
+  userID: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ type: [String], default: [], required: true })
+  documentReferences: string[];
+
+  @Prop({ type: [String], default: [], ref: 'User' })
+  participantIDs: string[];
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
+
+SchemaFactory.createForClass(Conversation);
+export const ConversationSchema = SchemaFactory.createForClass(Conversation);
+export type ConversationDocument = HydratedDocument<Conversation>;
+
+ConversationSchema.index({ userID: 1 });
---
+++ apps/backend/src/schemas/Message.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class Message {
+  @Prop({ ref: 'Conversation' })
+  conversationID: string;
+
+  @Prop({ ref: 'User' })
+  senderID: string;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
+
+SchemaFactory.createForClass(Message);
+export const MessageSchema = SchemaFactory.createForClass(Message);
+export type MessageDocument = HydratedDocument<Message>;
+
+MessageSchema.index({ conversationID: 1, senderID: 1 });
