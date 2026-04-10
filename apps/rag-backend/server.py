368d0f0ec89243bdabf82061ceabdc81e9e6727d
Hairum Qureshi <hqureshi@udel.edu>
2026-04-09 21:20:50 -0400

    updated process_pdfs POST endpoint so it takes in a payload BaseModel since it no longer just takes in URL string arrays and added a chunking function to chunk the text extracted from an uploaded PDF

--- apps/rag-backend/server.py
+++ apps/rag-backend/server.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+import requests, pdfplumber
+from io import BytesIO 
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -6,9 +10,40 @@ app = FastAPI()
 def root():
     return {"message": "Server is up and running!"}
 
+def chunk_text(text:str, chunk_size:int, overlap:int) -> list[str]:
+    chunks: list[str] = []
+    stride = chunk_size - overlap
+    curr_idx = 0
+
+    while curr_idx < len(text):
+        chunk = text[curr_idx:curr_idx + chunk_size]
+        chunks.append(chunk)
+        curr_idx += stride
+    
+    return chunks
+
+def get_pdf_content(url:str): 
+    response = requests.get(url)
+    response.raise_for_status()
+    text = ""
+    with pdfplumber.open(BytesIO(response.content)) as pdf:
+        for page in pdf.pages:
+           text += page.extract_text() or ""
+    chunks = chunk_text(text, chunk_size=1000, overlap=200)
+    print(chunks)
+
+class ProcessPDFRequest(BaseModel):
+    urls: List[str]
+    conversationID: str
+    userID: str
+
 @app.post('/process-pdfs')
-def process_pdfs(urls: list[str]):
-    print(urls)
-    return {"message": "PDF URLs received and processed successfully!"}
+def process_pdfs(payload: ProcessPDFRequest):
+    for url in payload.urls:
+        if requests.get(url).status_code == 200:
+            text_content = get_pdf_content(url)
+        else:
+            print(f"Failed to access PDF at URL: {url}")
+    return {"message": "PDF URLs processed successfully."}
 
 # To run this server, use the command: uvicorn server:app --reload
