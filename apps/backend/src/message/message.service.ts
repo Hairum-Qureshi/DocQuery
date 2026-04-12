2a3e7ca7f2b2ce3265001ddb53f31ed078b3a364
Hairum Qureshi <hqureshi@udel.edu>
2026-04-11 20:06:37 -0400

    updated getAllMessagesForConversation method database query so it only filters to get messages per conversation and not with the sender being the current user ID as well

--- apps/backend/src/message/message.service.ts
+++ apps/backend/src/message/message.service.ts
@@ -4,6 +4,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Conversation, ConversationDocument } from 'src/schemas/Conversation';
 import { Message } from 'src/schemas/Message';
+import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MessageService {
@@ -12,6 +14,7 @@ export class MessageService {
     private readonly messageModel: Model<MessageDocument>,
     @InjectModel(Conversation.name)
     private readonly conversationModel: Model<ConversationDocument>,
+    private readonly configService: ConfigService,
   ) {}
 
   async addMessageToConversation(
@@ -19,19 +22,30 @@ export class MessageService {
     conversationID: string,
     message: string,
   ) {
-    return await this.messageModel.create({
+    await this.messageModel.create({
       conversationID,
       senderID: currUserID,
       content: message,
     });
+
+    const response = await axios.post(
+      `${this.configService.get<string>('PYTHON_BACKEND')}/user-query/${conversationID}`,
+      {
+        user_id: currUserID,
+        query: message,
+      },
+    );
+
+    return await this.messageModel.create({
+      conversationID,
+      senderID: '0000000000000001',
+      content: response.data.answer,
+    });
   }
 
-  async getAllMessagesForConversation(
-    currUserID: string,
-    conversationID: string,
-  ) {
+  async getAllMessagesForConversation(conversationID: string) {
     return await this.messageModel
-      .find({ conversationID, senderID: currUserID })
+      .find({ conversationID })
       .sort({ createdAt: 1 })
       .lean();
   }
