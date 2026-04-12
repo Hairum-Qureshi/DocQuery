174612e9f4c8bbc73483b8fef3143835506626cc
Hairum Qureshi <hqureshi@udel.edu>
2026-04-11 23:09:15 -0400

    added useQuery hook call for getting conversation data by ID

--- apps/frontend/src/hooks/useConversation.ts
+++ apps/frontend/src/hooks/useConversation.ts
@@ -1,12 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import type { Conversation } from "../interfaces";
+import { useParams } from "react-router-dom";
 
 interface UseConversationHook {
 	conversationsList: Conversation[] | undefined;
+	conversationData: Conversation | undefined;
 }
 
 export default function useConversation(): UseConversationHook {
+	const { convoID } = useParams();
+
 	const { data: conversationsList } = useQuery({
 		queryKey: ["conversations-list"],
 		queryFn: async () => {
@@ -24,5 +28,22 @@ export default function useConversation(): UseConversationHook {
 		}
 	});
 
-	return { conversationsList };
+	const { data: conversationData } = useQuery({
+		queryKey: ["conversation", convoID],
+		queryFn: async () => {
+			try {
+				const response = await axios.get(
+					`${import.meta.env.VITE_BACKEND_URL}/conversation/${convoID}`,
+					{
+						withCredentials: true
+					}
+				);
+				return response.data;
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	});
+
+	return { conversationsList, conversationData };
 }
